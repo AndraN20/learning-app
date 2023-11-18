9a7df17c95147fb69af7cc4efd32b4b0f9208d8a
Aleksandru <mischie.andrey@gmail.com>
2023-11-18 14:44:13 +0200

    fixed some bugs, just some

--- src/app/components/text-file/text-file.component.ts
+++ src/app/components/text-file/text-file.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { PATHS } from 'src/app/constants/paths';
+import { TextFile } from 'src/app/types/text-file.type';
 
 @Component({
   selector: 'app-text-file',
   templateUrl: './text-file.component.html',
-  styleUrls: ['./text-file.component.css']
+  styleUrls: ['./text-file.component.css'],
 })
 export class TextFileComponent {
-  constructor(private readonly router: Router) {
+  @Input() file!: TextFile;
 
-  }
+  constructor(private readonly router: Router) {}
 
   redirectToChooseMethod(): void {
     this.router.navigate([PATHS.CHOOSEMETHOD]);
--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { TextFile } from 'src/app/types/text-file.type';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
 })
-export class HomepageComponent {
-  data = [
-    1, 2, 3, 4, 5, 6, 7
-  ]
+export class HomepageComponent implements OnInit {
+  constructor(private localStorageService: LocalStorageService) {}
+  files: TextFile[] = [];
+  ngOnInit(): void {
+    this.files = this.localStorageService.getFiles();
+  }
 }
