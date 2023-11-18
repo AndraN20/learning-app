5edc5556e7abd19e48f46f80685cccad4c323dda
teuo <teodorabanta@yahoo.com>
2023-11-18 23:34:03 +0200

    form for multiple-choice method

--- src/app/pages/multiple-choice/multiple-choice.component.ts
+++ src/app/pages/multiple-choice/multiple-choice.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MultipleChoice } from 'src/app/types/multiple-choice.type';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-multiple-choice',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./multiple-choice.component.css']
 })
 export class MultipleChoiceComponent {
+  multiplechoice: MultipleChoice[]=[{
+    question: "Cine a scris Moara cu Noroc?",
+    answers: [{sentence:"Ioan Slavici", value: true},{sentence: "Mihai Eminescu", value: false},{sentence: "Vasile Alecsandri", value: false},{sentence: "George Bacovia", value: false}]
+  },
+ { question: "Cine a scris Moara cu Noroc?",
+  answers: [{sentence:"Ioan Slavici", value: true},{sentence: "Mihai Eminescu", value: false},{sentence: "Vasile Alecsandri", value: false},{sentence: "George Bacovia", value: false}]
+},
+{question: "Cine a scris Moara cu Noroc?",
+answers: [{sentence:"Ioan Slavici", value: true},{sentence: "Mihai Eminescu", value: false},{sentence: "Vasile Alecsandri", value: false},{sentence: "George Bacovia", value: false}]
+},
+{question: "Cine a scris Moara cu Noroc?",
+answers: [{sentence:"Ioan Slavici", value: true},{sentence: "Mihai Eminescu", value: false},{sentence: "Vasile Alecsandri", value: false},{sentence: "George Bacovia", value: false}]
+}];
+ 
 
 }
+
---
+++ src/app/types/answer.type.ts
@@ -0,0 +1,4 @@
+export interface Answer{
+    sentence: string;
+    value: boolean;
+}
