525d9c8e927f72c0c1963950702c16b0b00580b6
Mihai-Simedrea <mihai.nicolae.simedrea@gmail.com>
2023-11-18 23:45:14 +0200

    Blank Page

--- src/app/pages/fill-the-blanks/fill-the-blanks.component.ts
+++ src/app/pages/fill-the-blanks/fill-the-blanks.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
 import { OpenAIService } from 'src/app/services/openai.service';
 
 @Component({
@@ -6,10 +7,30 @@ import { OpenAIService } from 'src/app/services/openai.service';
   templateUrl: './fill-the-blanks.component.html',
   styleUrls: ['./fill-the-blanks.component.css']
 })
-export class FillTheBlanksComponent {
-  constructor(private readonly openAIService: OpenAIService) {}
-  generateText(): void {
-    this.openAIService.generateText('salut').subscribe(result => {
+export class FillTheBlanksComponent implements OnInit {
+  constructor(private readonly openAIService: OpenAIService,
+              private readonly localStorageService: LocalStorageService) {}
+
+  wordsArray = [];
+  allContent: string = '';
+  allWords: any;
+
+  ngOnInit(): void {
+    this.openAIService.generateFillBlanksWords().subscribe(result => {
+      const message = result.choices[0].message.content;
+      console.log(message);
+      const filename = this.localStorageService.getCurrentFileName();
+      const file = this.localStorageService.getFile(filename);
+      let content: any = file?.text;
+      this.wordsArray = message.split(',');
+      this.wordsArray.forEach((word: string) => {
+        const regex = new RegExp('\\b' + word.trim() + '\\b', 'gi');
+        content = content.replace(regex, 'placeholder');
+      });
+      this.allContent = content;
+      this.allWords = this.allContent.split(/[ \n\t.?!]+/);
+      console.log(this.wordsArray);
+      console.log(this.allWords);
     });
   }
 }
