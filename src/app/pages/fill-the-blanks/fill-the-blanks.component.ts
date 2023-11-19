de55e23074a81694fa58ad0fe8d69338011f37e5
Mihai-Simedrea <mihai.nicolae.simedrea@gmail.com>
2023-11-19 12:57:09 +0200

    Add Fill The Blanks

--- src/app/pages/fill-the-blanks/fill-the-blanks.component.ts
+++ src/app/pages/fill-the-blanks/fill-the-blanks.component.ts
@@ -27,56 +27,28 @@ export class FillTheBlanksComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    // this.openAIService.generateFillBlanksWords().subscribe((result) => {
-    // });
-
-    // const message = result.choices[0].message.content;
-    const filename = this.localStorageService.getCurrentFileName();
-    const file = this.localStorageService.getFile(filename);
-    let content: any = file?.text;
-    // this.blankWords = JSON.parse(message);
-    let message = `[
-      {"word": "Catelus", "index": 0},
-      {"word": "parul", "index": 2},
-      {"word": "cret", "index": 3},
-      {"word": "Fura", "index": 4},
-      {"word": "rata", "index": 6},
-      {"word": "cotet", "index": 7},
-      {"word": "El", "index": 8},
-      {"word": "jura", "index": 9},
-      {"word": "fura", "index": 10},
-      {"word": "Si", "index": 11},
-      {"word": "prins", "index": 13},
-      {"word": "rata-n", "index": 15},
-      {"word": "gura", "index": 16},
-      {"word": "ou-n", "index": 18},
-      {"word": "buzunar", "index": 19},
-      {"word": "Hai", "index": 21},
-      {"word": "Sfatul", "index": 23},
-      {"word": "Popular", "index": 24},
-      {"word": "ma", "index": 26},
-      {"word": "duc", "index": 27},
-      {"word": "o", "index": 28},
-      {"word": "data", "index": 29},
-      {"word": "cazut", "index": 31},
-      {"word": "nasu", "index": 33},
-      {"word": "balta", "index": 35}
-    ]    
-    `
-    this.blankWords = JSON.parse(message);
-    console.log(this.blankWords);
-    this.allContent = content;
-    this.wordsArray = this.allContent.split(/[ \n\t.?!]+/);
-    this.blankWords.forEach(element => {
-      this.wordsArray[element.index] = `--placeholder-${element.index}-`;
-    });
-    console.log(this.blankWords);
-    console.log(this.wordsArray);
     this.form = this.fb.group({});
 
-    this.blankWords.forEach(word => {
-      const controlName = `control-${word.index}`;
-      this.form.addControl(controlName, this.fb.control(''));
+    this.openAIService.generateFillBlanksWords().subscribe((result) => {
+      const message = result.choices[0].message.content;
+      const filename = this.localStorageService.getCurrentFileName();
+      const file = this.localStorageService.getFile(filename);
+      let content: any = file?.text;
+      
+      this.blankWords = JSON.parse(message);
+      console.log(this.blankWords);
+      this.allContent = content;
+      this.wordsArray = this.allContent.split(/[ \n\t.?!]+/);
+      this.blankWords.forEach(element => {
+        this.wordsArray[element.index] = `--placeholder-${element.index}-`;
+      });
+      console.log(this.blankWords);
+      console.log(this.wordsArray);
+
+      this.blankWords.forEach(word => {
+        const controlName = `control-${word.index}`;
+        this.form.addControl(controlName, this.fb.control(''));
+      });
     });
   }
 
